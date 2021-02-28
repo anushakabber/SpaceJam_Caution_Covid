637f73389e6271b5bd876b8539fc0f1c5af1e64e
anusha2904 <60822709+anusha2904@users.noreply.github.com>
2021-02-28 08:05:33 +0530

    Adding to index.js

--- static/js/index.js
+++ static/js/index.js
@@ -44,11 +44,13 @@ $(document).ready(function(){
               // faceapi.draw.drawDetections(canvas, fullFaceDescriptions)
 
               const labels = ["Beyonce", "Jennifer", "Brad"]
+              const details = {'Beyonce' : ['Negative', 'Not_vaccinated', 'lives in hotspot'], 'Jennifer': ['Positive', 'Not_vaccinated', 'lives in hotspot'], 'Brad': ['Negative', 'Vaccinated', 'lives in hotspot'] }
 
               const labeledFaceDescriptors = await Promise.all(
                   labels.map(async label => {
                       // fetch image data from urls and convert blob to HTMLImage element
                       const imgUrl = `http://127.0.0.1:5000/static/LabeledImages/${label}/${label}.jpg`
+                      const c19_details = `http://127.0.0.1:5000/static/LabeledImages/${label}/${label}_details.jpg`
                       const img = await faceapi.fetchImage(imgUrl)
                       
                       // detect the face with the highest score in the image and compute it's landmarks and face descriptor
@@ -68,14 +70,45 @@ $(document).ready(function(){
 
               const results = fullFaceDescriptions.map(fd => faceMatcher.findBestMatch(fd.descriptor))
 
+              
+
+              var echoDB;
+
+              // Query echoAR
+              fetch('https://console.echoar.xyz/query?key=aged-mud-3260')
+              .then((response) => response.json())
+              .then((json) => {
+              console.log(json)
+                // Store database
+              echoDB = json; // The JSON response
+              })
+              .catch((error) => {
+              console.error(error);
+              });
+              
+              // var db = Object.values(echoDB.db);
+
               results.forEach((bestMatch, i) => {
                   const box = fullFaceDescriptions[i].detection.box
-                  const text = bestMatch.toString()
-                  const drawBox = new faceapi.draw.DrawBox(box, { label: text })
+                  var text = bestMatch.toString().split(" (")
+                  var total = text[0] 
+                  console.log(total+" total")
+                  for (const [key, value] of Object.entries(details)) {
+                    console.log(key+"key")
+                    var n = key.localeCompare(total);
+                    
+                    if (n == 0)
+                    {
+                      
+                      total = text[0] + "\n" + value[0] + "\n" + value[1] + "\n" + value[2];
+                      
+                    }
+                  }
+                  const drawBox = new faceapi.draw.DrawBox(box, { label: total })
                   drawBox.draw(canvas)
               })
 
-          },300);
+          },100);
 
           console.log(displaySize)
       }
